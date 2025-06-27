d5fdb7a84b797995155cd5bc4ee8a584758e2800
KavyaJP <kavya31052005@gmail.com>
2025-06-27 21:06:58 +0530

    added a feature when you close the app, it makes you requthenticate

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,30 @@ class AuthGate extends StatefulWidget {
   State<AuthGate> createState() => _AuthGateState();
 }
 
-class _AuthGateState extends State<AuthGate> {
+class _AuthGateState extends State<AuthGate> with WidgetsBindingObserver {
   bool _unlocked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      // App sent to background
+      setState(() {
+        _unlocked = false;
+      });
+    }
+  }
 
   void _onAuthenticated() {
     setState(() {
@@ -41,6 +63,7 @@ class _AuthGateState extends State<AuthGate> {
         : LockScreen(onAuthenticated: _onAuthenticated);
   }
 }
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
